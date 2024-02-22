0bdccaa0a49eb9c460882072621c1f0fa2d303d5
Marcio <augusto_sena@hotmail.com>
2024-02-21 23:18:19 -0300

    implementa fluxo do repository. teste ok!

--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -4,18 +4,22 @@ import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
 import br.com.petz.clientepet.cliente.application.api.ClienteResponse;
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteApplicationService implements ClienteService {
     private final ClienteRepository clienteRepository;
 
     @Override
     public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
         log.info("[inicia] ClienteApplicationService - criaCliente");
+        clienteLogger.logNovoCliente(clienteRequest);
         Cliente cliente = clienteRepository.salva(new Cliente(clienteRequest));
+        clienteTopicoProducer.notificaNovoCliente(cliente);
         log.info("[finaliza] ClienteApplicationService - criaCliente");
         return ClienteResponse.builder()
                 .idCliente(cliente.getIdCliente())
