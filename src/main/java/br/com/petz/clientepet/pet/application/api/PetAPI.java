e9132ab3327560eb1367c045971cc376b00344f5
Marcio <marcio.augusto.o.s@gmail.com>
2024-04-20 19:18:32 -0300

    implementa camada de API

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetAPI.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetAPI.java
@@ -28,4 +28,9 @@ public interface PetAPI {
     @DeleteMapping(value = "/{idPet}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     void deletePetDoClienteComId(@PathVariable UUID idCliente, @PathVariable UUID idPet);
+
+    @PatchMapping(value = "/{idPet}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    PetResponse patchPet(@PathVariable UUID idCliente, @PathVariable UUID idPet,
+                         @Valid @RequestBody PetAlteracaoRequest petAlteracaoRequest);
 }
