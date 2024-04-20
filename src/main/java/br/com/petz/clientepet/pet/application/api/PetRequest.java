53ab3cb1c3aa3ab258c452436f901445f403fa59
Marcio <marcio.augusto.o.s@gmail.com>
2024-04-20 15:15:39 -0300

    implementa fluxo da API

---
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetRequest.java
@@ -0,0 +1,7 @@
+package br.com.petz.clientepet.pet.application.api;
+
+import lombok.Value;
+
+@Value
+public class PetRequest {
+}
---
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetResponse.java
@@ -0,0 +1,9 @@
+package br.com.petz.clientepet.pet.application.api;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class PetResponse {
+}
