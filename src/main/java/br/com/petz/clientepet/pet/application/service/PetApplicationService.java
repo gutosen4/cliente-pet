73b3affecfc3f715034ad3c4ad13aa4aadc71a2f
Marcio <marcio.augusto.o.s@gmail.com>
2024-04-20 17:27:13 -0300

    implementa repository

--- src/main/java/br/com/petz/clientepet/pet/application/service/PetApplicationService.java
+++ src/main/java/br/com/petz/clientepet/pet/application/service/PetApplicationService.java
@@ -1,5 +1,6 @@
 package br.com.petz.clientepet.pet.application.service;
 
+import br.com.petz.clientepet.cliente.application.service.ClienteService;
 import br.com.petz.clientepet.pet.application.api.PetRequest;
 import br.com.petz.clientepet.pet.application.api.PetResponse;
 import br.com.petz.clientepet.pet.domain.Pet;
@@ -13,10 +14,16 @@ import java.util.UUID;
 @Log4j2
 @RequiredArgsConstructor
 public class PetApplicationService implements PetService {
+
+    private final ClienteService clienteService;
+    private final PetRepository petRepository;
+
     @Override
     public PetResponse criaPet(UUID idCliente, PetRequest petRequest) {
         log.info("[inicia] PetApplicationService - criaPet");
+        clienteService.buscaClienteAtravesId(idCliente);
+        Pet pet = petRepository.salvaPet(new Pet(idCliente, petRequest));
         log.info("[finaliza] PetApplicationService - criaPet");
-        return null;
+        return new PetResponse(pet.getIdPet());
     }
 }
--- src/main/java/br/com/petz/clientepet/pet/domain/Pet.java
+++ src/main/java/br/com/petz/clientepet/pet/domain/Pet.java
@@ -1,5 +1,6 @@
 package br.com.petz.clientepet.pet.domain;
 
+import br.com.petz.clientepet.pet.application.api.PetRequest;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -19,6 +20,9 @@ public class Pet {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(columnDefinition = "uuid", name = "idPet", updatable = false, unique = true, nullable = false)
     private UUID idPet;
+    @NotNull
+    @Column(columnDefinition = "uuid", name = "idClienteTutor", nullable = false)
+    private UUID idClienteTutor;
     @NotBlank
     private String nomePet;
     @Enumerated(EnumType.STRING)
@@ -40,4 +44,19 @@ public class Pet {
 
     private LocalDateTime dataHoraDoCadastro;
     private LocalDateTime dataHoraDaUltimaAlteracao;
+
+    public Pet(UUID idCliente, PetRequest petRequest) {
+        this.idClienteTutor = idCliente;
+        this.nomePet = petRequest.getNomePet();
+        this.porte = petRequest.getPorte();
+        this.microchip = petRequest.getMicrochip();
+        this.tipo = petRequest.getTipo();
+        this.raca = petRequest.getRaca();
+        this.sexo = petRequest.getSexo();
+        this.pelagemCor = petRequest.getPelagemCor();
+        this.dataNascimento = petRequest.getDataNascimento();
+        this.rga = petRequest.getRga();
+        this.peso = petRequest.getPeso();
+        this.dataHoraDoCadastro = LocalDateTime.now();
+    }
 }
